ee4e52339da28d5b0f07612822fe3f58b467c2f5
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-28 20:42:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require "pry"
+
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_list = {}
+  pigeon_data.each do |attribute, options|
+    options.each do |option, pigeons|
+      #binding.pry
+      pigeons.each do |pigeon|
+        #binding.pry
+        pigeon_list[pigeon] ||= {}
+        pigeon_list[pigeon][attribute] ||= []
+        pigeon_list[pigeon][attribute] << option.to_s
+        #binding.pry
+      end
+    end
+  end
+  pigeon_list
 end
