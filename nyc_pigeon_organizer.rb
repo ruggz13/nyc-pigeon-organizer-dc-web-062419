ee7deec0e99932482d26a9a7c2dea7f067755690
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-28 20:35:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ require "pry"
 
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_list = {}
-  pigeon_data.each do |attributes|
+  pigeon_data.each do |attribute, options|
+    options.each do |option, pigeons|
     binding.pry
   end
 end
