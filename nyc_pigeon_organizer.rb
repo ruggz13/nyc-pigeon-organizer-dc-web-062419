d965b8cfcb72aa07257d7fcfb421bca1ab6208a0
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-28 20:40:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,9 @@ def nyc_pigeon_organizer(pigeon_data)
   pigeon_list = {}
   pigeon_data.each do |attribute, options|
     options.each do |option, pigeons|
-      binding.pry
+      #binding.pry
       pigeons.each do |pigeon|
+        binding.pry
         pigeon_list[pigeon] ||= {}
         pigeon_list[pigeon][attribute] ||= []
         pigeon_list[pigeon][attribute] << option.to_s
